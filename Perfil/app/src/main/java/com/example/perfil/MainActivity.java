dbbb4ea830b386c756cbbffb41c9877f816b2649
BorjaOteroFerreira <115078003+BorjaOteroFerreira@users.noreply.github.com>
2024-10-05 01:53:04 +0200

    .

--- Perfil/app/src/main/java/com/example/perfil/MainActivity.java
+++ Perfil/app/src/main/java/com/example/perfil/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnVolver), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -36,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sp = getSharedPreferences("sp.xml", MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         String nombre = sp.getString("nombre", "Hola amigo");
-        lblNombre.setText(nombre);
+        lblNombre.setText("¡Hola " + nombre + "!");
 
     }
 
