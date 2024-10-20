22d8143b02a25246ed2941814e7ecdd094699785
BorjaOteroFerreira <115078003+BorjaOteroFerreira@users.noreply.github.com>
2024-10-20 21:21:34 +0200

    Login

---
+++ Login/app/src/main/java/com/example/login/AsistenteBD.java
@@ -0,0 +1,76 @@
+package com.example.login;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class AsistenteBD extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "usaurios.db";
+    private static final int DATABASE_VERSION = 1;
+    private static  AsistenteBD instance = null;
+
+    public AsistenteBD(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public static synchronized AsistenteBD getInstance(Context context) {
+        if (instance == null) {
+            instance = new AsistenteBD(context.getApplicationContext());
+        }
+        return instance;
+    }
+
+    public ArrayList<String> getUsuarioYcontraseña(String usuario, String contraseña) {
+        SQLiteDatabase db = getWritableDatabase();
+        ArrayList<String> datos = new ArrayList<>();
+        String[] args = new String[]{usuario, contraseña};
+        String[] campos = new String[]{"nombre", "password"};
+        String where = "nombre = ? AND password = ?";
+        Cursor cursor = db.query("usuarios", campos, where, args, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                datos.add(cursor.getString(0));
+                datos.add(cursor.getString(1));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return datos;
+    }
+
+
+    public void insertarUsuarioInicial() {
+        SQLiteDatabase db = getWritableDatabase();
+        String countQuery = "SELECT COUNT(*) FROM usuarios";
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.moveToFirst();
+        int count = cursor.getInt(0);
+        cursor.close();
+        if (count == 0) {
+            db.execSQL("INSERT INTO usuarios (nombre, apellido, email, password) VALUES ('admin', 'adminApellido', 'admin@admin', 'admin123')");
+        }
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE usuarios (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "nombre TEXT," +
+                "apellido TEXT," +
+                "email TEXT UNIQUE," +
+                "password TEXT" +
+                ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS usuarios");
+        onCreate(db);
+
+    }
+
+
+}
+
---
+++ Login/app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.login;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.snackbar.Snackbar;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText etNombre, etPassword;
+    AsistenteBD asistenteBd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        asistenteBd = AsistenteBD.getInstance(this);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        etNombre = findViewById(R.id.etName);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        asistenteBd.insertarUsuarioInicial();
+        btnLogin.setOnClickListener(v -> login());
+    }
+
+    private void login() {
+        String nombre = etNombre.getText().toString();
+        String password = etPassword.getText().toString();
+        AsistenteBD asistenteBD = AsistenteBD.getInstance(this);
+        ArrayList<String> usuario = asistenteBD.getUsuarioYcontraseña(nombre, password);
+        if (usuario.size() < 1) {
+            Snackbar.make(findViewById(R.id.main), "Login Incorrecto", Snackbar.LENGTH_LONG).show();
+        } else if (password.equals(usuario.get(1)) && nombre.equals(usuario.get(0))) {
+                Snackbar.make(findViewById(R.id.main), "Login correcto", Snackbar.LENGTH_LONG).show();
+            }
+        }
+}
